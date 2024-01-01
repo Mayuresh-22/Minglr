7cf51378872b925969a2d91bd4d779cccc466f50
Akash Tiwari <132299371+ak4631@users.noreply.github.com>
2024-01-01 19:41:29 +0530

    Fixing Logout SWOC
    Fixing Logout Functionality of Minglr

--- back/logout.php
+++ back/logout.php
@@ -1,18 +1,13 @@
 <?php
-	include("connection.php");
     include("env.php");
 
     session_start();
-    $uid = $_SESSION['id'];
+    
 
     // validation before loggin user out
     if(isset($_SESSION['username'])){
         session_unset();
         session_destroy();
-
-        // set login status in database
-        $sql = "UPDATE `users` SET `status`= 0 WHERE `id`=".$uid.";";
-        mysqli_query($connection, $sql);
 
         echo '
         <script>
