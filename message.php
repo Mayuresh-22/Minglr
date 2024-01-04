7fce86ead35a10dcd9c9ea6e178ff3b08e2d11ad
Gauri Agrawal <118062523+GauriGA@users.noreply.github.com>
2024-01-04 21:11:16 +0530

    Update message.php

--- message.php
+++ message.php
@@ -78,6 +78,7 @@ if(mysqli_num_rows($result) == 1){
     <!-- favicon -->
     <link rel="shortcut icon" href="img/favicon_minglr.png" type="image/png">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-ZvHjXoebDRUrTnKh9WKpWV/A0Amd+fjub5TkBXrPxe5F7WfDZL0slJ6a0mvg7VSN3qdpgqq2y1blz06Q8W2Y8A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="https://kit.fontawesome.com/17a4e5185f.js" crossorigin="anonymous"></script>
     </head>
 
@@ -203,21 +204,37 @@ if(mysqli_num_rows($result) == 1){
     </div>
 
     <div class="footer">
-    <ul>
-            <li class="foot-item">
-                <a href="" class="foot-link" style="text-decoration: none">Home</a>
-            </li>
-            <li class="foot-item">
-                <a href="feed.php" class="foot-link" style="text-decoration: none">Feed</a>
-            </li>
-            <li class="foot-item">
-                <a href="account.php" class="foot-link" style="text-decoration: none">Account</a>
-            </li>
-            <li class="foot-item">
-                <a href="about-us.php" class="foot-link" style="text-decoration: none">About us</a>
-            </li>
-        </ul>
-    </div>
+    <ul class="footer-icons">
+        <li class="foot-item">
+            <a href="#" class="foot-link"><i class="fab fa-facebook"></i></a>
+        </li>
+        <li class="foot-item">
+            <a href="#" class="foot-link"><i class="fab fa-twitter"></i></a>
+        </li>
+        <li class="foot-item">
+            <a href="#" class="foot-link"><i class="fab fa-instagram"></i></a>
+        </li>
+        <li class="foot-item">
+            <a href="#" class="foot-link"><i class="fab fa-youtube"></i></a>
+        </li>
+    </ul>
+    <ul class="footer-links">
+        <li class="foot-item">
+            <a href="" class="foot-link">Home</a>
+        </li>
+        <li class="foot-item">
+            <a href="feed.php" class="foot-link">Feed</a>
+        </li>
+        <li class="foot-item">
+            <a href="account.php" class="foot-link">Account</a>
+        </li>
+        <li class="foot-item">
+            <a href="about-us.php" class="foot-link">About us</a>
+        </li>
+    </ul>
+    <p>This website is only for educational purposes and does not try to replicate any institution/entity/company - by Mayuresh Choudhary</p>
+</div>
+
     <script src="js/script.js"></script>
 </body>
 </html>
