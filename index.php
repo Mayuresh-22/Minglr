e3cca40b67e97477493906c11cb8255c226c898f
Amine2000s <92222814+Amine2000s@users.noreply.github.com>
2024-01-25 06:06:20 +0100

    added js confirmation logic

--- index.php
+++ index.php
@@ -121,6 +121,50 @@ session_start();
                 <input type="password" for="password" id="password" name="password" placeholder="Password" required>
                 <small>Your data will be used to provide you with the seamless experience. We respect your privacy</small>
                 <button class="rgst-btn" name="regst" id="regst">Register</button>
+
+                <!-- Handle password input -->
+                <script>
+                    const passwordInput = document.getElementById('pass');
+                    const registerButton = document.getElementById('regst');
+                    //only for password
+                    const toggleButton = document.getElementById('togglePassword');
+                    const kindOfPassword = document.getElementById('kindOfPassword');
+
+                    passwordInput.addEventListener("input", () => {
+                        //empty password field
+                        if (passwordInput.value === "") {
+                            passwordInput.classList.remove('valid-password', 'invalid-password');
+                            registerButton.disabled = true;
+                            registerButton.style.cursor = "not-allowed";    //change cursor to not-allowed
+                            toggleButton.hidden = true;
+                            kindOfPassword.hidden = true;
+                        } else {    //non-empty password field
+                            const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/; //means a-z, A-Z, 0-9, @$!%*?& and min 8 characters
+                            if (passwordPattern.test(passwordInput.value)) {    //check if password is valid
+                                passwordInput.classList.remove('invalid-password');
+                                passwordInput.classList.add('valid-password');
+                                registerButton.disabled = false;     
+                                registerButton.style.cursor = "pointer";   //enable register button    
+                                toggleButton.hidden = false;            //hide password toggle button
+                                kindOfPassword.hidden = false;
+                            } else {    //invalid password
+                                passwordInput.classList.remove('valid-password');
+                                passwordInput.classList.add('invalid-password');
+                                registerButton.disabled = true;                 //disable register button
+                                registerButton.style.cursor = "not-allowed";    //change cursor to not-allowed
+                                toggleButton.hidden = false;
+                                kindOfPassword.hidden = false;
+                            }
+                        }
+                    });
+                    //toggle password visibility
+                    toggleButton.addEventListener('click', (e) => {
+                        e.preventDefault();
+                        const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+                        passwordInput.setAttribute('type', type);
+                        toggleButton.textContent = type === 'password' ? 'Show' : 'Hide';
+                    });
+                </script>
             </form>
         </div>
     </div>
